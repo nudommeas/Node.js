01b7d12438acdf250413366f164d7f469b9a512f
Nudom <measthannudom@gmail.com>
2025-12-02 16:41:31 +0700

    Converting HTML files to PUG

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/admin',admin.routes);
 app.use(shop);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404');
     
 })
 
